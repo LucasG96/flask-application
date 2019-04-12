c619c2064c7b097c37aea1813a9815e4cf7eb516
Lucas Gomes <lucas.sist.inf@gmail.com>
2019-04-12 17:31:54 -0300

    Corrigida inversão das iformações de getter e setter

--- Orientação a Objetos/Módulo 1/cliente.py
+++ Orientação a Objetos/Módulo 1/cliente.py
@@ -2,12 +2,12 @@ class Cliente:
     def __init__(self, valor):
         self.__nome = valor
 
-    # Funciona como se fosse o setter, funciona chamando o atributo assim: cliente.nome
+    # Funciona como se fosse o getter, funciona chamando o atributo assim: cliente.nome
     @property
     def nome(self):
         return self.__nome.title()
 
-    # Funciona como se fosse o getter, funciona chamando o atributo assim: cliente.nome = "Seu Nome"
+    # Funciona como se fosse o setter, funciona chamando o atributo assim: cliente.nome = "Seu Nome"
     @nome.setter
     def nome(self, valor):
         self.__nome = valor
